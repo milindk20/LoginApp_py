9c82594520e2101072b45143e30ee747ec84dd5f
Milind Kumar <milindkr707@gmail.com>
2024-09-14 00:01:10 +0530

    create new user is now live and successful

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import time
 from flask import Flask, render_template, redirect, url_for, request, session, flash
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
@@ -33,6 +34,18 @@ app.config['MYSQL_DB'] = db_dbname
 
 mysql = MySQL(app)
 
+#======================================Custom Functions=======================================================
+def session_check():
+    if session['logged_in'] == True:
+        check=1
+        print(check)
+    else:
+        check=0
+    return check
+#===========================================================================================================
+
+
+
 # Home route
 @app.route('/')
 def home():
@@ -52,7 +65,7 @@ def login():
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM users WHERE username = %s AND password = %s', (username, password))
         user = cursor.fetchone()
-
+        print(session)
         if user:
             session['logged_in'] = True
             session['username'] = user['username']
@@ -62,11 +75,49 @@ def login():
     
     return render_template('login.html')
 
+# create_user route
+@app.route('/create_user', methods=['GET', 'POST'])
+def redirect_to_create_user():
+    if session_check()==1:
+        return render_template('create_user.html')
+    elif session_check() !=1:
+            return redirect(url_for('login'))
+
+
+# create_new_user route
+@app.route('/create_new_user', methods=['GET', 'POST'])
+def create_new_user():
+    if session_check()==1:
+        if request.method == 'POST':
+            username = request.form['username']
+            email = request.form['email']
+            password = request.form['password']
+
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+            #Checkes if username or email exists already
+            #if  (cursor.execute('SELECT count(1) FROM users WHERE username = %s', (username)) >=1):
+             #   flash(f'Username {username} already exists!! please try a different one!!')
+           # if  (cursor.execute('SELECT count(1) FROM users WHERE email = %s', (email)) >=1):
+            #    flash(f'email {email} already exists!! please try a different one!!')
+            # Query database to insert the new user exists
+#            else:    
+            cursor.execute('INSERT INTO users (username,email, password) VALUES (%s,%s, %s);', (username,email, password))
+            cursor.execute('commit')
+            flash(f'User "{username}" ha been created successfully!!','success')
+        return render_template('create_user.html')
+    
+    elif session_check() !=1:
+            return redirect(url_for('login'))
+
 # Logout route
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     return redirect(url_for('login'))
 
+
+
+# StartApp
 if __name__ == '__main__':
     app.run(debug=True)
